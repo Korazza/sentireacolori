676623a373b914bb9d93314fae4d0a8f6858018a
Korazza <corattiroberto@gmail.com>
2023-02-16 17:38:08 +0100

    chore: add facebook auth provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,14 @@
 import NextAuth from 'next-auth'
+import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
 	secret: process.env.NEXTAUTH_SECRET,
 	providers: [
+		FacebookProvider({
+			clientId: process.env.FACEBOOK_ID!,
+			clientSecret: process.env.FACEBOOK_SECRET!,
+		}),
 		GoogleProvider({
 			clientId: process.env.GOOGLE_ID!,
 			clientSecret: process.env.GOOGLE_SECRET!,
