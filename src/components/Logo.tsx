a5808dd5337fe4213324896665014d120159ef19
Korazza <corattiroberto@gmail.com>
2023-02-20 13:58:40 +0100

    chore: fix font loading

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,10 @@
-import localFont from '@next/font/local'
-
 import styles from '@/styles/Logo.module.css'
-
-const kgHappy = localFont({ src: '../assets/KGHAPPY.ttf' })
 
 export default function Logo() {
 	return (
 		<div className={styles.logo}>
 			<span className={styles.logoText1}>Sentire</span>
-			<span className={`${styles.logoText2} ${kgHappy.className}`}>
+			<span className={styles.logoText2}>
 				{' '}
 				a colori
 			</span>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
-import localFont from '@next/font/local'
-
 import Navbar from '@/components/Navbar'
 import styles from '@/styles/Home.module.css'
-
-const kgHappy = localFont({ src: '../assets/KGHAPPY.ttf' })
 
 export default function Home() {
 	return (
 		<main className='container'>
 			<section className={styles.section1}>
 				<Navbar />
-				<div className={`${styles.content1} ${kgHappy.className}`}>
+				<div className={styles.content1}>
 					Impariamo a conoscere
 					<br />
 					le nostre emozioni insieme
