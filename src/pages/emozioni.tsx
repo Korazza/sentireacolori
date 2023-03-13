c0d1bf623e74955ccd0ca3b09d3fa113a18be746
Korazza <corattiroberto@gmail.com>
2023-03-13 11:49:41 +0100

    chore: make emozioni content responsive

--- src/pages/emozioni.tsx
+++ src/pages/emozioni.tsx
@@ -20,52 +20,39 @@ const EMOTIONS: Emotion[] = [
 		name: 'la tristezza',
 		content: `<p>
 		Non esiste una vita senza tristezza, è quasi impossibile non
-		<br />
-		sperimentarlaalmeno una volta, ed è giusto provarla.
-		<br />
+		sperimentarla almeno una volta, ed è giusto provarla.
 		<strong>
 			Lei ci aiuta a crescere, a maturare, a capire quando è
-			<br />
 			il momento di prenderci un momento per noi.
 		</strong>
 		Lei ci aiuta a
-		<br />
 		rimanere con i piedi per terra, perché, siamo sinceri avita non è
 		per
-		<br />
 		niente una passeggiata. Nella vita
 		<i>
 			è difficile che non si sperimenti
-			<br />
+			
 			la tristezza.
 		</i>
 	</p>
 	<p>
 		Facendo un esempio banale: da bambini anche quando si chiedeva di
-		<br />
 		mangiare un gelato, e i nostri genitori, o i nostri nonni ci
 		rispondevano di
-		<br />
 		no, diventavamo tristi, arrabbiati perché noi in quel momento
-		<br />
 		desideravamo mangiarlo, lo volevamo a tutti i costi. Vero?
-		<br />
 		Anche solo il fatto di amare qualcuno, di poterlo perdere, ci rende
 		tristi.
-		<br />
 		La tristezza non è mai assoluta o sempre presente o sempre assente,
-		<br />
 		arriva a tratti, a tratti fa male, a tratti menomale. Da piccoli si
 		tendeva a
-		<br />
 		dimostrare di più la nostra tristezza, magari piangendo, urlando,
 		facendo
-		<br />i capricci, e
+		i capricci, e
 		<strong>
 			<u>
 				<i>
 					da piccoli hanno sempre tentato di distrarci, di farci
-					<br />
 					pensare a delle cose belle che tristi, a farci ridere.
 				</i>
 			</u>
@@ -77,11 +64,8 @@ const EMOTIONS: Emotion[] = [
 		name: 'la rabbia',
 		content: `<p>
 		La rabbia è uno di quelle emozioni che ci insegnano a guardarla
-		<br />
     come una cosa sbagliata. Io dico che non lo sia, è giusto provarle
-		<br />
     tutte, No? Sono quel che sono. Okay, la rabbia su alcune persone
-		<br />
     sfocia in ira, ma..
 	</p>
 	<p>
@@ -94,28 +78,20 @@ const EMOTIONS: Emotion[] = [
 		<u>
 			l’ira è una brutta cosa da gestire lo so, è tosta.
 		</u>
-		<br />
 		Come ho letto in un libro
 		<strong>
 			è una specie di DRAGO dormiente
-			<br />
 			la rabbia,
 		</strong>
 		che vive nella profondità dentro di noi, ma quando esce
-		<br />
 		e non ne abbiamo l’assoluto controllo, eccola la che arriva l’ira,
-		<br />
 		quella che ti fa dire e fare cose che non vorresti.
 	</p>
 	<p>
 		La rabbia è un pugno in pieno stomaco, ci fa dire cose che magari
-		<br />
 		non pensiamo realmente, ci fare azioni che non vorremmo compiere,
-		<br />
 		la rabbia è una delle emozioni più forti che difficilmente non
-		<br />
 		proveremmo mai. La rabbia è un fuoco che tutti noi abbiamo, chi più
-		<br />
 		alimentato, chi più tenue, affievolito. Ma mai va sottovalutato.
 	</p>`,
 	},
@@ -124,40 +100,30 @@ const EMOTIONS: Emotion[] = [
 		name: 'la felicità',
 		content: `<p>
 		Sappiamo tutti che la vita è come una specie di altalena, delle
-		<br />
     volte abbiamo dei picchi di felicità, delle volte è troppo lenta..
 	</p>
 	<p>
 		<strong>
 		Ma poi dove sarebbe il divertimento se fosse sempre e
-		<br />
 		solo così? Diventerebbe monotona.
 		</strong>
 	</p>
 	<p>
 		La vita ci prende a calci in faccia ogni giorno, ci mette davanti
-		<br />
 		davvero tanti ostacoli, e le emozioni negative sono quelle che la
-		<br />
 		maggior parte del tempo prendonoil sopravvento.
 	</p>
 	<p>
 		Perciò sta a noi prendere in mano la situazione e cambiarla,
-		<br />
 		rovesciare una situazione del cavolo e renderla quantomeno
-		<br />
 		sopportabile.
 		<strong>
 			La felicità assoluta non esiste! Scordati di essere
-			<br />
 			ogni giorno felice..
 		</strong>
 	  Okay detta così è sicuramente sbagliato, ci
-		<br />
 		saranno sicuramente periodi in cui sarai felice eh però è una
-		<br />
 		specie di altalena.. Oscilliamo da un’emozione all’altra, ed è
-		<br />
 		giusto così.
 	</p>`,
 	},
